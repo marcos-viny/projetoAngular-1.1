b0c339f6f1183d71450edb6389c2ad56d25b6a5e
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2022-01-20 19:11:53 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projetoAngular-1.1';
-}
+export class AppComponent implements OnInit{
+
+  constructor() {window.addEventListener('scroll', this.handleScroll)};
+
+  ngOnInit(): void{}
+
+
+  handleScroll(){
+    const animationClass = "animate";
+     const allSubMenus = document.querySelectorAll("[data-anima]");
+     const windowTop = window.pageYOffset + window.innerHeight * 0.75;
+     for(const sub of allSubMenus as any){
+       if(windowTop > sub.offsetTop){
+        sub.classList.add(animationClass);
+     }
+     }   
+  }
+};
